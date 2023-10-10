91d9f95e86aa9c4efb310df3e0a0695d465f072d
jhokk <jayhokkanen25@gmail.com>
2023-10-10 01:19:44 -0400

    test

--- main.js
+++ main.js
@@ -5,19 +5,23 @@ import { Pane } from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.1/dist/tweakpan
 const shader = `${seagulls.constants.vertex}
 
 @group(0) @binding(0) var<uniform> frame : f32;
+@group(0) @binding(1) var<uniform> resolution : vec2f;
 
 @fragment 
 fn fs( @builtin(position) pos : vec4f ) -> @location(0) vec4f {
   let value = .5 + sin( frame / 60. ) * .5;
-  return vec4f( value, 0., 1.-value, 1. );
+  return vec4f( p.x, p.y, value, 1. );
 }`
 
-const sg = await seagulls.init()
+async function main() {
+    const sg = await seagulls.init()
+    let frame = 0
 
-let frame = 0
+    sg.uniforms({ frame: 0, resolution:[window.innerWidth, window.innerHeight] })
 
-sg.uniforms({ frame:0 })
+    sg.onframe(() => sg.uniforms.frame = frame++)
 
-sg.onframe( ()=> sg.uniforms.frame = frame++ )
+    sg.render(shader).run()
+}
 
-sg.render( shader ).run()
+main()
