7d62c32de32c95f43f93a2913474777973ee2492
jhokk <jayhokkanen25@gmail.com>
2023-10-10 00:45:46 -0400

    test

---
+++ main.js
@@ -0,0 +1,14 @@
+import { default as seagulls } from 'seagulls-main/seagulls.js'
+import { default as Mouse }    from 'seagulls-main/helpers/mouse.js'
+import { Pane } from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.1/dist/tweakpane.min.js';
+
+const shader = `${seagulls.constants.vertex}
+
+@fragment 
+fn fs( @builtin(position) pos : vec4f ) -> @location(0) vec4f {
+  return vec4f( 1.,0.,0. , 1. );
+}`
+
+const sg = await seagulls.init()
+
+sg.render( shader ).run()
